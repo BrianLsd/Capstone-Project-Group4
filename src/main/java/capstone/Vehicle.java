f7879e606e295f68dc1fe553ddbd71da834b8dba
mogopaly87 <mogononso@gmail.com>
2023-03-01 11:49:39 -0330

    added author javadoc

--- src/main/java/capstone/Vehicle.java
+++ src/main/java/capstone/Vehicle.java
@@ -1,17 +1,30 @@
 package capstone;
 
+/**
+ * Create Vehicle objects having a year, model and make of the vehicle
+ */
 public class Vehicle {
 
     private int year;
     private String model;
     private String make;
 
+    /**
+     * A Constructor to create vehicle objects
+     * @param year The vehicle year
+     * @param model The vehicle model
+     * @param make The vehicle make
+     */
     public Vehicle(int year, String model, String make) {
         this.year = year;
         this.model = model;
         this.make = make;
     }
 
+    /**
+     * Gets the year of a vehicle object
+     * @return year
+     */
     public int getYear() {
         return year;
     }
